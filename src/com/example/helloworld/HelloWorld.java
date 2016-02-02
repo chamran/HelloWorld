616ecf86cd578855214aafc5f522d719e6689051
Ran Yim <cyim@madisoncollege.edu>
2016-02-01 21:44:43 -0600

    Second file to share from IntelliJ

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by Student on 1/28/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
